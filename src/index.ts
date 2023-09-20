62502d9c7ac88bf2ab6249338b0f17c748139346
Lucas c. Marinho <lucasc.marinho@hotmail.com>
2023-09-20 16:37:12 -0300

    implementing seeds

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import { server } from "./server/server";
+import { Knex } from "./server/database/knex";
 
-server.listen(process.env.PORT || 3333, () => console.log(`Servidor rodando na porta ${process.env.PORT || 3333}`));
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => console.log(`Servidor rodando na porta ${process.env.PORT || 3333}`));
+};
+
+if (process.env.IS_LOCALHOST !== "true") {
+  Knex.migrate.latest()
+    .then(() => {
+      Knex.seed.run()
+        .then(() => startServer());
+    }).catch(console.log);
+} else {
+  startServer();
+}
